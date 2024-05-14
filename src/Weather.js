28a2d079ca36c578ff94d0a4104cff9dbef4595d
Derek Radcliffe <dereksradcliffe@gmail.com>
2024-05-13 22:10:03 -0400

    Updated to useRef and added animations

--- src/Weather.js
+++ src/Weather.js
@@ -1,60 +1,55 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useRef } from 'react';
 import axios from 'axios';
 import './styles.css';
 import { Button, Header, Grid, GridColumn, GridRow, Container } from 'semantic-ui-react';
 
 const Weather = () => {
   const [weatherData, setWeatherData] = useState(null);
-  const [lat, setLat] = useState([]);
-  const [long, setLong] = useState([]);
+  const lat = useRef(null);
+  const long = useRef(null);
 
   const fetchData = async () => {
     try {
       const response = await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&appid=${process.env.REACT_APP_ID}`
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat.current}&lon=${long.current}&units=imperial&appid=${process.env.REACT_APP_ID}`
       );
       setWeatherData(response.data);
-      console.log(response.data);
     } catch (error) {
       console.error(error);
     }
   };
 
-  useEffect(() => {
+  const handleGetLocation = () => {
     navigator.geolocation.getCurrentPosition(function (position) {
-      setLat(position.coords.latitude);
-      setLong(position.coords.longitude);
+      lat.current = position.coords.latitude;
+      long.current = position.coords.longitude;
+      fetchData();
     });
-  }, [lat, long]);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    fetchData();
   };
 
   return (
     <div>
-      <Button primary className="ui button" type="submit" onClick={handleSubmit}>Lets Find Out</Button>
+      <Button primary className="ui button" type="submit" onClick={handleGetLocation}>Let's Find Out</Button>
 
       {weatherData ? (
         <>
-          <Header as='h1' id="pageSubheader">Currently in {weatherData.name}</Header>
+          <Header as='h1' id="pageSubheader" className='slide-top'>Currently in {weatherData.name}</Header>
 
           <Grid columns={3}>
             <GridRow>
               <GridColumn>
                 <Container>
-                  <div className='weatherData'>Temperature: {Math.round(weatherData.main.temp)}°F</div>
+                  <div className='weatherData slide-top'>Temperature: {Math.round(weatherData.main.temp)}°F</div>
                 </Container>
               </GridColumn>
               <GridColumn>
                 <Container>
-                  <div className='weatherData'>Description: {weatherData.weather[0].description}</div>
+                  <div className='weatherData slide-top'>Description: {weatherData.weather[0].description}</div>
                 </Container>
               </GridColumn>
               <GridColumn>
                 <Container>
-                  <div className='weatherData'>Feels like : {Math.round(weatherData.main.feels_like)}°F</div>
+                  <div className='weatherData slide-top'>Feels like : {Math.round(weatherData.main.feels_like)}°F</div>
                 </Container>
               </GridColumn>
             </GridRow>
@@ -62,17 +57,17 @@ const Weather = () => {
             <GridRow>
               <GridColumn>
                 <Container>
-                  <div className='weatherData'>Humidity : {weatherData.main.humidity}%</div>
+                  <div className='weatherData slide-top'>Humidity : {weatherData.main.humidity}%</div>
                 </Container>
               </GridColumn>
               <GridColumn>
                 <Container>
-                  <div className='weatherData'>Pressure : {weatherData.main.pressure}</div>
+                  <div className='weatherData slide-top'>Pressure : {weatherData.main.pressure}</div>
                 </Container>
               </GridColumn>
               <GridColumn>
                 <Container>
-                  <div className='weatherData'>Wind Speed : {Math.round(weatherData.wind.speed)} mph</div>
+                  <div className='weatherData slide-top'>Wind Speed : {Math.round(weatherData.wind.speed)} mph</div>
                 </Container>
               </GridColumn>
             </GridRow>
